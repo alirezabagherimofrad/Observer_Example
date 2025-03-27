04755aa17f2925e2e9afd3a04d1adecbc5b3908f
smzomorodian <smzomorodian80@gmail.com>
2025-03-27 09:17:04 +0330

    Update Program.cs دیدی؟

--- Observer_Example/Program.cs
+++ Observer_Example/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            YouTubeChannel channel = new YouTubeChannel();
+
+            User user1 = new User("Alice");
+
+            User user2 = new User("Bob");
+
+          
+            channel.Subscribe(user1);
+
+            channel.Subscribe(user2);
+
+          
+            channel.UploadVideo("Observer Pattern Explained");
+
+            channel.Unsubscribe(user2);
+
+            channel.UploadVideo("Design Patterns in C#");
             
         }
     }
