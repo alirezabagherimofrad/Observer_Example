d3138b9b4a223094b608f87f0fa0f9b4c006d441
dash bagher <dash bagher@DESKTOP-BO3LEGK>
2025-03-27 08:51:02 +0330

    Add project files.

---
+++ Observer_Example/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Observer_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ Observer_Example/YouTubeChannel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer_Example
+{
+    public class YouTubeChannel
+    {
+        private List<ISubscriber> subscribers = new List<ISubscriber>();
+
+        public void Subscribe(ISubscriber subscriber)
+        {
+            subscribers.Add(subscriber);
+        }
+
+        public void Unsubscribe(ISubscriber subscriber)
+        {
+            subscribers.Remove(subscriber);
+        }
+
+        public void UploadVideo(string title)
+        {
+            Console.WriteLine($"New video uploaded : {title}");
+            NotifySubscribers(title);
+        }
+
+        private void NotifySubscribers(string videoTitle)
+        {
+            foreach (var subscriber in subscribers)
+            {
+                subscriber.Update(videoTitle);
+            }
+        }
+    }
+}
