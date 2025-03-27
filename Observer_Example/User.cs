f6d6afca11054b935d211e9fb98cb35e64b7d59e
smzomorodian <smzomorodian80@gmail.com>
2025-03-27 09:18:09 +0330

    Update User.cs

--- Observer_Example/User.cs
+++ Observer_Example/User.cs
@@ -8,21 +8,7 @@ namespace Observer_Example
 {
     public class User
     {
-// ایجاد پخش‌کننده فیلم
-        MoviePlayer moviePlayer = new MoviePlayer();
 
-        // ایجاد Observerها (ویدیو، صدا، زیرنویس)
-        Video video = new Video();
-        Audio audio = new Audio();
-        Subtitles subtitles = new Subtitles();
-
-        // ثبت Observerها در پخش‌کننده فیلم
-        moviePlayer.AddObserver(video);
-        moviePlayer.AddObserver(audio);
-        moviePlayer.AddObserver(subtitles);
-
-        // پخش فیلم
-        moviePlayer.PlayMovie();
         
     }
 }
